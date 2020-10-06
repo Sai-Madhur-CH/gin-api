5eff7721f341fb0794c596caad119e511994cddd
Sai Madhur CH <saimadhurch@gmail.com>
2020-10-06 23:07:29 +0530

    Added comments

--- controllers/userController.go
+++ controllers/userController.go
@@ -7,6 +7,10 @@ import (
 )
 
 
+// In the controller get the actual request params or request body
+// And call the services
+// get the service response and handle error if any
+// And finally return the service response
 func UserRegister(c *gin.Context) {
 	result, err := userService.UserRegister()
 	if err != nil {
--- services/userService.go
+++ services/userService.go
@@ -1,5 +1,6 @@
 package services
 
+// Main bussiness logic is implemented in the service
 func UserRegister() (map[string]interface{},error) {
 	result := make(map[string]interface{})
 	result["name"] = "This is Sai"
